2b653a60fa1504352323d362e9a367e2fe2bcbd0
Mehedi Hassan <mehedi15-5815@diu.edu.bd>
2023-01-31 13:30:14 +0600

    Practice Home Design complete

--- view-encapsulation/src/app/app.module.ts
+++ view-encapsulation/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
+import { ParentComponent } from './parent/parent.component';
+import { HomeComponent } from './home/home.component';
+
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'parent', component: ParentComponent },
+  { path: 'child', component: ChildComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    ParentComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    [RouterModule.forRoot(appRoutes)]
   ],
   providers: [],
   bootstrap: [AppComponent]
